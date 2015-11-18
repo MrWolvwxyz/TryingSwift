c811ce217cab60e6f354b2e56d65a736d4e1e562
Sam Bean <sabean@umich.edu>
2015-11-17 20:14:56 -0500

    Initial Commit

---
+++ TryingSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TryingSwift
+//
+//  Created by samuel bean on 11/17/15.
+//  Copyright (c) 2015 samuel bean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
